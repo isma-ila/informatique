b9765797a775bc5b0440108d3110f2cd0c73e058
isma-ila <105105884+isma-ila@users.noreply.github.com>
2024-01-12 16:00:53 +0100

    add structure mvc "jeu_de_la_vie"

---
+++ LiveModel.py
@@ -0,0 +1,84 @@
+import copy
+
+
+class LiveModel:
+    #__word_original = ["B", "O", "N", "J", "O", "U", "R"]
+
+    # taille de la grille
+    __height = 0
+    __width = 0
+
+    # taille des cellules
+    __c = 0
+
+    # dictionnaire contenant les coordonnées de chaques cellules et une valeur 0 ou 1 si elles sont respectivement mortes ou vivantes
+    __dico_case = {}
+
+    # vitesse de l'animation (en réalité __c'est l'attente entre chaque étapes en ms)
+    __vitesse = 0
+
+    # dictionnaire contenant le nombre de cellules vivantes autour de chaque cellule
+    __dico_etat = {}
+
+    #
+    __flag = 0
+
+
+    def __init__(self):
+        #self.__word = copy.copy(self.__word_original)
+        self.__height = 200
+        self.__width = 200
+        self.__c = 10
+        self.__dico_case = copy.copy(self.__dico_case)
+        self.__dico_etat = copy.copy(self.__dico_etat)
+        self.__flag = copy.copy(self.__flag)
+        self.__vitesse = 50
+
+
+
+    """
+        def get_word(self):
+		#Retourne les données sous forme de liste
+		
+        return self.__word
+    """
+
+    # Getter height
+    def get_height(self):
+        return self.__height
+
+    # Getter width
+    def get_width(self):
+        return self.__width
+
+    # Getter cellules
+    def get_c(self):
+        return self.__c
+
+    def get_dico_case(self):
+        return self.__dico_case
+
+    def get_dico_etat(self):
+        return self.__dico_etat
+
+    def get_flag(self):
+        return self.__flag
+
+    def get_vitesse(self):
+        return self.__vitesse
+
+    def set_flag(self, flag):
+        self.__flag = flag
+
+    def set_vitesse(self, vitesse):
+        self.__vitesse = vitesse
+
+    """
+        def modify_word(self, index):
+        code = ord(self.__word[index])
+        new_letter = chr(code + 1)
+        self.__word[index] = new_letter
+
+    def reset_word(self):
+        self.__word = copy.copy(self.__word_original)
+    """
---
+++ Main_livegame_mvc.py
@@ -0,0 +1,4 @@
+from LiveController import LiveController
+
+if __name__ == '__main__':
+    LiveController()
